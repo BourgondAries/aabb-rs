4ab7399593bc4786844e76ea06c30c9a987e0683
Bourgond Aries <macocio@gmail.com>
2016-03-30 10:57:14 +0200

    Order floats and create tests.

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
-extern crate toml;
+extern crate sfml;
 
-fn get_file_contents(filename: &str) -> std::io::Result<String> {
-	use std::fs::File;
-	use std::io::Read;
-	let mut file = try!(File::open(filename));
-	let mut string = String::new();
-	try!(file.read_to_string(&mut string));
-	Ok(string)
-}
+mod float_order;
+mod test;
+
+use float_order::Float;
 
 fn main() {
-	use toml::Value;
-	let y = &String::from("ok");
-	let mut x = &String::from("what");
-	x = y;
-	match *x {
-		ref y => println!("{}", y),
-	}
-	// use livetoml::livetoml::Interpret;
-	let string = get_file_contents("test.toml").unwrap();
-	let mut table: Value = string.parse()
-		.unwrap();
-	// table.eval("name.0 = derp").expect("");
-	println!("{:?}", table.lookup("name.0"));
-	println!("{:?}", table);
+	use std::collections::BTreeMap;
+	let mut map: BTreeMap<Float, f32> = BTreeMap::new();
 }
+
